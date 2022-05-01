86443bb8eb32c2c0260ebdf745e5d59f7c3d0110
하지현 <kkim.haji@gmail.com>
2022-05-01 19:29:55 +0900

    activate tab

--- midex/app.js
+++ midex/app.js
@@ -1,4 +1,26 @@
-console.log('app');
-console.log(popularContents);
-console.log(viewContents);
-console.log(recentContents);
+const $tabs = document.querySelectorAll("li");
+const $list = document.getElementById("list");
+
+const tabName = {0: "recent", 1: "view", 2: "popular"};
+let tabNum = 0;
+let selectedTab = $tabs[tabNum];
+
+window.onload = () => {
+    selectTab();
+}
+function activateTab(tab) {
+    selectedTab.classList.remove("active");
+    tab.classList.add("active");
+    selectedTab = tab;
+    const tabNum = Array.from($tabs).indexOf(tab);
+    return tabNum;
+}
+
+function selectTab() {
+    for (const $tab of $tabs) {
+        $tab.addEventListener("click", (event) => {
+            $list.innerHTML = "";
+            tabNumber = activateTab(event.target.parentElement);
+        });
+    }
+}
